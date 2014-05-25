2e1df4d93a3c67a4583c4dfdaee1549659197a58
Ketoth Xupack <ketoth.xupack@gmail.com>
2014-05-25 13:36:11 +0700

    game2048 demo fix
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

--- apps/Game2048/2048.js
+++ apps/Game2048/2048.js
@@ -1,1 +1,30 @@
-var $webfx = {title: "2048 Game Demo", i18n: null};
+
+var EventHandler = Packages.javafx.event.EventHandler;
+var Direction = Packages.game2048.Direction;
+var KeyCode = Packages.javafx.scene.input.KeyCode;
+
+var handler = new EventHandler() {
+    handle: function(e) {
+        var keyCode = e.getCode();
+        if (keyCode.equals(KeyCode.W)) {
+            gameManager.move(Direction.UP);
+        }
+        if (keyCode.equals(KeyCode.S)) {
+            gameManager.move(Direction.DOWN);
+        }
+        if (keyCode.equals(KeyCode.A)) {
+            gameManager.move(Direction.LEFT);
+        }
+        if (keyCode.equals(KeyCode.D)) {
+            gameManager.move(Direction.RIGHT);
+        }
+    }
+}
+
+var $webfx = {
+    title: "2048 Game Demo",
+    i18n: null,
+    initWebFX : function() {
+        $webfx.scene.setOnKeyPressed(handler)
+    }
+};
